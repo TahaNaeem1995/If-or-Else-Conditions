a7c93a5800f936599ab03ba3b8c506d3df03d190
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 15:52:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// let isRaining = false;
+// if (isRaining){
+//     console.log("wear a raincoat.");
+// }else{
+//     console.log("wear sunglasses.");
+// }
+// let isCold = true;
+// if (isCold){
+//     console.log("wear a sweater.");
+// }else{
+//     console.log("wear T-shirt." );
+// }
+// let num1 = 6
+// if (num1  == 6){
+//     console.log("number is 6");
+// }else if(num1 == 8){
+//     console.log("Number is not 8");
+// }
+// else if (num1 == 10){
+//     console.log("Number is right");
+// }
+var num1 = 10;
+if (num1 == 10) 
+//10 ==10
+{
+    console.log("Hello Im Taha");
+}
+else {
+    console.log("under else");
+}
